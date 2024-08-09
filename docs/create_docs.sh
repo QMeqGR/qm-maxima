fbcb80923c13e89e0dbb79c861991966a4895133
Eric Majzoub <eric.majzoub@umsystem.edu>
2024-08-09 06:42:35 -0500

    update docs and README

--- docs/create_docs.sh
+++ docs/create_docs.sh
@@ -5,5 +5,6 @@ packname=qm
 makeinfo $packname.texi;
 texi2pdf $packname.texi;
 texi2html $packname.texi;
+texi2any --plaintext $packname.texi > ../README.md;
 
 rm $packname.aux $packname.fn $packname.fns $packname.log $packname.toc
