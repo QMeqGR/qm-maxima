b98333c3c7dd214313be787b4f2d5479598e98d8
Eric Majzoub <eric.majzoub@umsystem.edu>
2024-08-10 17:53:45 -0500

    work in progress on regen examples

--- docs/regen_examples.sh
+++ docs/regen_examples.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
 
-maxima --batch-string="load(qm); brap(bra(a,g));"
+packname=qm;
+
+cat $packname.texi | gawk -f ./regen_examples.awk > tmp.regen.1;
+
+icount_list=$(cat tmp.regen.1 | gawk '($1~/rgen-icount/){printf("%s ",$2);}');
+comm_list=$(cat tmp.regen.1 | \
+		gawk '($1~/rgen-icount/){for(i=4;i<NF+1;i++){printf("%s ",$i);}printf("\n");}')
+
+echo $icount_list
+echo $comm_list
+
+#maxima --batch-string="load(qm); brap(bra(a,g));"
 
