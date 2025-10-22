1a963b12f92789a3f8755e1c2a38e658e916b920
Eric Majzoub <eric.majzoub@umsystem.edu>
2025-10-22 11:26:13 -0500

    update regen_examples script

--- docs/regen_examples.sh
+++ docs/regen_examples.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
+# script version 2.0
+
+# Note: When maxima runs a batch file, the output contains the command line
+# but without the last 'termination' character. So ; and $ are lost. Thus we
+# have to do it here by saving those characters and re-inserting them. This
+# is done below in the function restore_lastchar().
+
 packname=""
 debug=0
 
+
+# Section A
 # get package name
 n=$(ls *.texi | wc -l | awk '{print $1}');
 if [ $n -gt 1 ]; then
@@ -22,6 +31,42 @@ if [ ! -f "$packname.texi" ]; then
     exit 2
 fi
 
+
+function restore_lastchar () {
+    count=0;
+    macfile=$1;
+    tmpfile=$2;
+    n_macfile=$(wc -l $macfile | awk '{print $1}');
+    n_tmpfile=$(wc -l $tmpfile | awk '{print $1}');
+    cp $tmpfile .tmp.recurse;
+    if [ $debug -ge 1 ]; then
+	echo "mac file lines: "$n_macfile;
+	echo "tmp file lines: "$n_tmpfile;
+    fi
+    # get last characters in the mac file, should be ; or $
+    cat $macfile | awk '(NR>3){print $0}' |
+	sed 's/[[:space:]]*$//;s/.*\(.\)/\1/' > .tmp.lastchar
+    
+    for line in $(seq 1 $n_tmpfile); do
+	is_input=$(cat $tmpfile | awk -v LN=$line \
+				      '(NR==LN){if($1~/\(%i[1-9]/){print 1}else{print 0}}');
+	if [ $debug -ge 1 ]; then 
+	    echo "is_input: "$is_input
+	fi
+	if [ $is_input -ge 1 ]; then
+	    count=$((count+1)); # increment count and get this line from the lastchar file
+	    lc=$(cat .tmp.lastchar | head -$count | tail -1);
+	    if [ $debug -ge 1 ]; then
+		echo "------------> last char lc is \"$lc\"        input line count: "$count "   line number: "$line
+		echo "              input line: "$(cat $tmpfile | head -$line | tail -1)
+	    fi
+	    cat .tmp.recurse | awk -v LC=$lc -v LN=$line \
+		   '(NR==LN && ($1~/\(%i[1-9]/)){printf("%s%s\n",$0,LC)}(NR!=LN){print $0}' > .tmp.junk
+	    mv -f .tmp.junk .tmp.recurse
+	fi
+    done
+    cat .tmp.recurse > .tmp.grp.$i.tmp4
+}
 
 ###########################################
 # awk script for generating markers
@@ -76,6 +121,8 @@ if [ $debug -gt 0 ]; then
     echo
 fi
 
+
+# Section B
 # All commands in the same group must be run as one batch file.
 
 ###################################################
@@ -86,9 +133,10 @@ for i in $(seq 1 $ngrps); do
     echo "display2d_unicode:false$" > .tmp.grp.$i.mac
     echo "load($packname)$" >> .tmp.grp.$i.mac
     echo "linenum:0$" >> .tmp.grp.$i.mac
-    echo "extracting examples from group "$i;
+    echo "################ extracting examples from group "$i "####################";
     cat tmp.regen.1 | awk -v G="$i" --source \
-      '($1~/grpcnt/ && $2==G){for(i=6;i<NF+1;i++){printf("%s ",$i);}printf("\n");}' >> .tmp.grp.$i.mac
+    '($1~/grpcnt/ && $2==G){for(i=6;i<NF+1;i++){printf("%s ",$i);}printf("\n");}' >> .tmp.grp.$i.mac;
+    # now we must capture the last character of the input commands to append in the final output
     # now batch run the file
     maxima -q --batch .tmp.grp.$i.mac > .tmp.grp.$i.tmp1
     # throw out any tellsimp warnings
@@ -99,8 +147,10 @@ for i in $(seq 1 $ngrps); do
     linenumline=$(grep -n linenum .tmp.grp.$i.tmp2 | awk -F: '{print $1}')
     cat .tmp.grp.$i.tmp2 | awk -v N="$nlines" -v NLL="$linenumline"\
 			       --source '(NR>NLL && NR<N){print $0}' > .tmp.grp.$i.tmp3
-    # Now post process the output
-    cat .tmp.grp.$i.tmp3 | awk '{if($1~/\(%i[1-9]/){printf("%s;\n",$0)}else{print $0}}' > .tmp.grp.$i.out
+    # Now post process the output to include the lastcharacter from above...
+    restore_lastchar .tmp.grp.$i.mac .tmp.grp.$i.tmp3;
+    cat .tmp.grp.$i.tmp4 > .tmp.grp.$i.out
+    #cat .tmp.grp.$i.tmp3 | awk '{if($1~/\(%i[1-9]/){printf("%s;\n",$0,$LC)}else{print $0}}' > .tmp.grp.$i.out
 done
 
 ####################################################
